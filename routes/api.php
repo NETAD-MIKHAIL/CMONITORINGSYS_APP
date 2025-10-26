be9f4c44a4ad2ba5065b371019eb75296fbabda0
mikee <chaelromo@gmail.com>
2025-10-26 20:52:01 +0800

    Clear route and config cache

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleSheetController;
 
-// Route for Google Sheet POST data
 Route::post('/google-sheet-data', [GoogleSheetController::class, 'store']);
